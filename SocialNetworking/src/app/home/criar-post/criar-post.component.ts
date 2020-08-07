5d751bf50d6e0728dadcf5169862cdfe8040ee85
Flakes <ed.afram@gmail.com>
2020-08-07 17:27:22 -0300

    criando posts

---
+++ SocialNetworking/src/app/home/criar-post/criar-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-post',
+  templateUrl: './criar-post.component.html',
+  styleUrls: ['./criar-post.component.scss']
+})
+export class CriarPostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- SocialNetworking/src/app/home/home.module.ts
+++ SocialNetworking/src/app/home/home.module.ts
@@ -4,6 +4,7 @@ import { LayoutModule } from '../layout/layout.module';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { HomeBlocoComponent } from './home-bloco/home-bloco.component';
+import { CriarPostComponent } from './criar-post/criar-post.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
@@ -11,7 +12,7 @@ const routes: Routes = [
 
 
 @NgModule({
-  declarations: [HomePageComponent, HomeBlocoComponent],
+  declarations: [HomePageComponent, HomeBlocoComponent, CriarPostComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
